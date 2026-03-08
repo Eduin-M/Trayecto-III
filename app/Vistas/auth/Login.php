f999ff391e38b31fc032e0b008b57e5f1c339567
Eduin-M <eduinmeneses86@gmail.com.com>
2026-03-08 14:46:19 -0400

    feat: eliminar controlador y vista de login

--- app/Vistas/auth/Login.php
+++
@@ -1,8 +0,0 @@
-<h2>Login de Prueba</h2>
-<form action="/trayectoiii/auth/ingresar" method="POST">
-    <input type="email" name="email" placeholder="admin@correo.com" required>
-    <br><br>
-    <input type="password" name="password" placeholder="123456" required>
-    <br><br>
-    <button type="submit">Entrar al Sistema</button>
-</form>
